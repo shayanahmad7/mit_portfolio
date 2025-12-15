a30f2d38cfd13d918c02496a39fea23e515d4b78
Shayan Ahmad <sa6097@nyu.edu>
2025-12-15 17:00:57 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -256,7 +256,7 @@ export default function About() {
                 </div>
               </motion.div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </section>
 
