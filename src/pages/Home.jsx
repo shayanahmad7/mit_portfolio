e87e7f04880b5c8fe5ee0fd1a46a39ca3c26b3b1
Shayan Ahmad <sa6097@nyu.edu>
2025-12-15 14:45:09 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -120,7 +120,7 @@ export default function Home() {
         />
         
         <div className="max-w-7xl mx-auto px-6 py-20 relative z-10">
-          <div className="grid lg:grid-cols-2 gap-16 items-center">
+          <div className="flex flex-col lg:grid lg:grid-cols-2 gap-16 items-center">
             {/* Left: Text */}
             <motion.div
               initial={{ opacity: 0, x: -50 }}
@@ -287,12 +287,12 @@ export default function Home() {
           <div className="grid lg:grid-cols-12 gap-12">
             {/* Vertical Title */}
             <motion.div 
-              className="lg:col-span-2 flex lg:flex-col items-center lg:items-start"
+              className="lg:col-span-2 flex items-start justify-start lg:justify-center"
               initial={{ opacity: 0, x: -30 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
             >
-              <h2 className="text-6xl md:text-8xl font-black opacity-5 lg:writing-mode-vertical lg:text-orientation-mixed whitespace-nowrap">
+              <h2 className="text-4xl md:text-6xl lg:text-8xl font-black text-black/10 lg:[writing-mode:vertical-lr] whitespace-nowrap">
                 THE STORY
               </h2>
             </motion.div>
